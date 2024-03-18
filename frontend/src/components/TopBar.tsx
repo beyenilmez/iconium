9f486db3cca42e73fa8b676a844577ac87e49b9b
Bedirhan Yenilmez <yenilmezbedirhan@gmail.com>
2024-03-18 11:36:43 +0300

    add pause icon to run button

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
 import { useProfile } from "@/contexts/profile-provider";
-import { GetProfiles, GetProfile, SyncDesktop, RemoveProfile, SetIcon, RunProfile } from "wailsjs/go/main/App";
+import { GetProfiles, GetProfile, SyncDesktop, RemoveProfile, RunProfile } from "wailsjs/go/main/App";
 import { Button } from "./ui/button";
 import { Combobox } from "./ui/combobox"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
-import { RefreshCw, Play, Plus } from 'lucide-react';
+import { RefreshCw, Play, Plus, Pause } from 'lucide-react';
 import { ModeToggle } from "./mode-toggle"
 import { CreateProfileForm } from "./CreateProfileForm"
 
 import { /* profile as profStruct, */ profileInfo, fileInfo } from "@/structs";
+import React from "react";
 
 const TopBar = () => {
     const { profile, setProfile } = useProfile();
+    const [running, setRunning] = useState(false);
 
     const [profiles, setProfiles] = useState<profileInfo[]>([]);
 
@@ -51,36 +53,35 @@ const TopBar = () => {
             </div>
             <div className="flex items-center space-x-2">
                 {profile.name && (
-                    <Button variant="destructive" size={"lg"} className="space-x-2" onClick={() => {
-                        RemoveProfile(profile.name);
-                        setProfile({ name: "", id: "", value: [] })
-                    }}>
-                        Remove Profile
-                    </Button>
+                    <React.Fragment>
+                        <Button variant="destructive" size={"lg"} className="space-x-2" onClick={() => {
+                            RemoveProfile(profile.name);
+                            setProfile({ name: "", id: "", value: [] })
+                        }}>
+                            Remove Profile
+                        </Button>
+                        <Button variant="outline" size={"lg"} className="space-x-2" onClick={() => {
+                            SyncDesktop(profile.name, true).then((profile) => setProfile(profile));
+                        }}>
+                            <RefreshCw />
+                            <div>
+                                Get Desktop
+                            </div>
+                        </Button>
+                        <Button variant="outline" size={"icon"} disabled={running}
+                         onClick={() => {
+                            const fileInfos: fileInfo[] = profile.value;
+
+                            setRunning(true);
+                            RunProfile(profile.name, fileInfos).then(() => setRunning(false));
+                        }}>
+                            <Play className={running ? "hidden" : ""}  />
+                            <Pause className={running ? "" : "hidden"} />
+                        </Button>
+                    </React.Fragment>
                 )}
-                <Button variant="outline" size={"lg"} className="space-x-2" onClick={() => {
-                    SyncDesktop(profile.name, true).then((profile) => setProfile(profile));
-                }}>
-                    <RefreshCw />
-                    <div>
-                        Get Desktop
-                    </div>
-                </Button>
-                <Button variant="outline" size={"icon"} onClick={() => {
-                    const fileInfos : fileInfo[] = profile.value;
 
-                    RunProfile(profile.name, fileInfos);
-                }}>
-                    <Play />
-                </Button>
                 <ModeToggle />
-                <Button variant={"secondary" } onClick={() => {
-                    const fileInfo : fileInfo[] = profile.value;
-
-                    SetIcon(profile.name, fileInfo[2]);
-                }}>
-                    Test
-                </Button>
             </div>
         </div>
     )
