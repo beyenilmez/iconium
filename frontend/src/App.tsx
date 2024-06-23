1bc2bc1bab3a218dd023aa0390bebf5aca098066
Bedirhan Yenilmez <yenilmezbedirhan@gmail.com>
2024-06-23 20:50:01 +0300

    add theme provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,11 @@
-import { Button } from "@/components/ui/button"
-import React from "react"
+import { ThemeProvider } from "./contexts/theme-provider";
 
 function App() {
-  const [count, setCount] = React.useState(0)
-
   return (
-    <div className="min-h-screen bg-white grid place-items-center mx-auto py-8">
-      <div className="text-blue-900 text-2xl font-bold flex flex-col items-center space-y-4">
-        <h1>Vite + React + TS + Tailwind + shadcn/ui</h1>
-        <Button onClick={() => setCount(count + 1)}>Count up ({count})</Button>
-      </div>
-    </div>
-  )
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <h1>Vite + React + TS + Tailwind + shadcn/ui</h1>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
