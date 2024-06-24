9556d93dd35c9e7da5240ae0cc8886ded82e5331
Bedirhan Yenilmez <yenilmezbedirhan@gmail.com>
2024-06-24 19:02:53 +0300

    add log filtering to advanced settings

--- frontend/src/components/Settings/AdvancedSettings.tsx
+++ frontend/src/components/Settings/AdvancedSettings.tsx
@@ -1,4 +1,19 @@
-import { GetEnableLogging, SetEnableLogging } from "wailsjs/go/main/App";
+import {
+  GetEnableLogging,
+  SetEnableLogging,
+  GetEnableTrace,
+  SetEnableTrace,
+  GetEnableDebug,
+  SetEnableDebug,
+  GetEnableInfo,
+  SetEnableInfo,
+  GetEnableWarn,
+  SetEnableWarn,
+  GetEnableError,
+  SetEnableError,
+  GetEnableFatal,
+  SetEnableFatal,
+} from "wailsjs/go/main/App";
 import {
   SettingsGroup,
   SettingsItem,
@@ -6,24 +21,58 @@ import {
   SettingDescription,
   SettingLabel,
 } from "../ui/settings-group";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { Switch } from "../ui/switch";
 import { useEffect, useState } from "react";
 
 export function AdvancedSettings() {
   const [enableLogging, setEnableLogging] = useState(false);
+  const [enableTrace, setEnableTrace] = useState(false);
+  const [enableDebug, setEnableDebug] = useState(false);
+  const [enableInfo, setEnableInfo] = useState(false);
+  const [enableWarn, setEnableWarn] = useState(false);
+  const [enableError, setEnableError] = useState(false);
+  const [enableFatal, setEnableFatal] = useState(false);
 
   useEffect(() => {
     GetEnableLogging().then((value) => {
       setEnableLogging(value);
     });
+
+    GetEnableTrace().then((value) => {
+      setEnableTrace(value);
+    });
+
+    GetEnableDebug().then((value) => {
+      setEnableDebug(value);
+    });
+
+    GetEnableInfo().then((value) => {
+      setEnableInfo(value);
+    });
+
+    GetEnableWarn().then((value) => {
+      setEnableWarn(value);
+    });
+
+    GetEnableError().then((value) => {
+      setEnableError(value);
+    });
+
+    GetEnableFatal().then((value) => {
+      setEnableFatal(value);
+    });
   }, []);
 
   return (
-    <SettingsGroup className="flex items-start px-4 py-2 w-full h-full">
+    <SettingsGroup className="flex flex-col items-start px-4 py-2 w-full h-full">
       <SettingsItem>
         <div>
           <SettingLabel>Logging</SettingLabel>
-          <SettingDescription>Enable logging to files.</SettingDescription>
+          <SettingDescription>
+            Enable logging to files. (Restart the app for the change to take
+            effect.)
+          </SettingDescription>
         </div>
         <SettingContent>
           <Switch
@@ -34,6 +83,90 @@ export function AdvancedSettings() {
               });
             }}
           />
+        </SettingContent>
+      </SettingsItem>
+
+      <SettingsItem>
+        <div>
+          <SettingLabel>Log levels</SettingLabel>
+          <SettingDescription>
+            Select the log levels that should be logged.
+          </SettingDescription>
+        </div>
+        <SettingContent>
+          <ToggleGroup type="multiple" value={[enableTrace ? "trace" : "", enableDebug ? "debug" : "", enableInfo ? "info" : "", enableWarn ? "warn" : "", enableError ? "error" : "", enableFatal ? "fatal" : ""]}>
+            <ToggleGroupItem
+              value="trace"
+              aria-label="Enable trace logging"
+              onClick={() => {
+                SetEnableTrace(!enableTrace).then(() => {
+                  setEnableTrace(!enableTrace);
+                });
+              }}
+            >
+              Trace
+            </ToggleGroupItem>
+
+            <ToggleGroupItem
+              value="debug"
+              aria-label="Enable debug logging"
+              onClick={() => {
+                SetEnableDebug(!enableDebug).then(() => {
+                  setEnableDebug(!enableDebug);
+                });
+              }}
+            >
+              Debug
+            </ToggleGroupItem>
+
+            <ToggleGroupItem
+              value="info"
+              aria-label="Enable info logging"
+              onClick={() => {
+                SetEnableInfo(!enableInfo).then(() => {
+                  setEnableInfo(!enableInfo);
+                });
+              }}
+            >
+              Info
+            </ToggleGroupItem>
+
+            <ToggleGroupItem
+              value="warn"
+              aria-label="Enable warn logging"
+              onClick={() => {
+                SetEnableWarn(!enableWarn).then(() => {
+                  setEnableWarn(!enableWarn);
+                });
+              }}
+            >
+              Warn
+            </ToggleGroupItem>
+
+            <ToggleGroupItem
+              value="error"
+              aria-label="Enable error logging"
+              onClick={() => {
+                SetEnableError(!enableError).then(() => {
+                  setEnableError(!enableError);
+                });
+              }}
+            >
+              Error
+            </ToggleGroupItem>
+
+            <ToggleGroupItem
+              value="fatal"
+              aria-label="Enable fatal logging"
+              onClick={() => {
+                SetEnableFatal(!enableFatal).then(() => {
+                  setEnableFatal(!enableFatal);
+                });
+              }}
+            >
+              Fatal
+            </ToggleGroupItem>
+          </ToggleGroup>
         </SettingContent>
       </SettingsItem>
     </SettingsGroup>
