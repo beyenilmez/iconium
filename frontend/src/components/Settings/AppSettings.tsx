404cb2647063065a2f2159d93341644608117c76
Bedirhan Yenilmez <yenilmezbedirhan@gmail.com>
2024-06-25 05:48:24 +0300

    move theme setting to general

--- frontend/src/components/Settings/AppSettings.tsx
+++ frontend/src/components/Settings/AppSettings.tsx
@@ -1,6 +1,4 @@
 import { GetConfigField, SetConfigField } from "wailsjs/go/main/App";
-import { Moon, Sun, Monitor } from "lucide-react";
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import {
   SettingsGroup,
   SettingsItem,
@@ -9,14 +7,12 @@ import {
   SettingLabel,
 } from "../ui/settings-group";
 import { Switch } from "@/components/ui/switch";
-import { useTheme } from "@/contexts/theme-provider";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 export function AppSettings() {
   const { t } = useTranslation();
 
-  const { theme, setTheme } = useTheme();
   const [useSystemTitleBar, setUseSystemTitleBar] = useState(false);
 
   useEffect(() => {
@@ -27,40 +23,6 @@ export function AppSettings() {
 
   return (
     <SettingsGroup className="flex flex-col items-start px-4 py-2 w-full h-full">
-      <SettingsItem>
-        <div>
-          <SettingLabel>{t("settings.application.theme.label")}</SettingLabel>
-          <SettingDescription>
-            {t("settings.application.theme.description")}
-          </SettingDescription>
-        </div>
-        <SettingContent>
-          <ToggleGroup type="single" value={theme}>
-            <ToggleGroupItem
-              value="system"
-              aria-label="Use system theme"
-              onClick={() => setTheme("system")}
-            >
-              <Monitor className="w-4 h-4" />
-            </ToggleGroupItem>
-            <ToggleGroupItem
-              value="light"
-              aria-label="Use light theme"
-              onClick={() => setTheme("light")}
-            >
-              <Sun className="w-4 h-4" />
-            </ToggleGroupItem>
-            <ToggleGroupItem
-              value="dark"
-              aria-label="Use dark theme"
-              onClick={() => setTheme("dark")}
-            >
-              <Moon className="w-4 h-4" />
-            </ToggleGroupItem>
-          </ToggleGroup>
-        </SettingContent>
-      </SettingsItem>
-
       <SettingsItem>
         <div>
           <SettingLabel>
--- frontend/src/components/Settings/GeneralSettings.tsx
+++ frontend/src/components/Settings/GeneralSettings.tsx
@@ -10,6 +10,9 @@ import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Combobox } from "../ui/combobox";
 import locales from "@/locales.json";
+import { ToggleGroup, ToggleGroupItem } from "../ui/toggle-group";
+import { Monitor, Moon, Sun } from "lucide-react";
+import { useTheme } from "@/contexts/theme-provider";
 
 export function GeneralSettings() {
   const { t, i18n } = useTranslation();
@@ -35,6 +38,8 @@ export function GeneralSettings() {
     }
   }, [language]);
 
+  const { theme, setTheme } = useTheme();
+
   return (
     <SettingsGroup className="flex flex-col items-start px-4 py-2 w-full h-full">
       <SettingsItem>
@@ -59,6 +64,40 @@ export function GeneralSettings() {
               "settings.general.language.no_languages_found"
             )}
           />
+        </SettingContent>
+      </SettingsItem>
+
+      <SettingsItem>
+        <div>
+          <SettingLabel>{t("settings.application.theme.label")}</SettingLabel>
+          <SettingDescription>
+            {t("settings.application.theme.description")}
+          </SettingDescription>
+        </div>
+        <SettingContent>
+          <ToggleGroup type="single" value={theme}>
+            <ToggleGroupItem
+              value="system"
+              aria-label="Use system theme"
+              onClick={() => setTheme("system")}
+            >
+              <Monitor className="w-4 h-4" />
+            </ToggleGroupItem>
+            <ToggleGroupItem
+              value="light"
+              aria-label="Use light theme"
+              onClick={() => setTheme("light")}
+            >
+              <Sun className="w-4 h-4" />
+            </ToggleGroupItem>
+            <ToggleGroupItem
+              value="dark"
+              aria-label="Use dark theme"
+              onClick={() => setTheme("dark")}
+            >
+              <Moon className="w-4 h-4" />
+            </ToggleGroupItem>
+          </ToggleGroup>
         </SettingContent>
       </SettingsItem>
     </SettingsGroup>
