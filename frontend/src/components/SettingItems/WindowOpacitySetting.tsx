58a640ff05ffcc4a7b77025657668b1b230393fd
Bedirhan Yenilmez <yenilmezbedirhan@gmail.com>
2024-07-03 23:13:59 +0300

    add generic config slider component

--- frontend/src/components/SettingItems/WindowOpacitySetting.tsx
+++ frontend/src/components/SettingItems/WindowOpacitySetting.tsx
@@ -1,72 +1,22 @@
-import {
-  SettingsItem,
-  SettingContent,
-  SettingDescription,
-  SettingLabel,
-} from "../ui/settings-group";
-import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Slider } from "../ui/my-slider";
-import { useConfig } from "@/contexts/config-provider";
+import { SliderConfig } from "./Presets/SliderConfig";
 
 export function WindowOpacitySetting() {
-  const { config, initialConfig, setConfigField } = useConfig();
   const { t } = useTranslation();
-  const [isLoading, setIsLoading] = useState(true);
-  const [useOpacity, setUseOpacity] = useState(-1);
-
-  useEffect(() => {
-    if (config && config.opacity !== undefined && isLoading) {
-      setUseOpacity(config.opacity);
-
-      setIsLoading(false);
-    }
-  }, [config?.opacity]);
-
-  const handleOpacityChange = (value: number) => {
-    document.documentElement.style.setProperty(
-      "--opacity",
-      String(value / 100)
-    );
-    setUseOpacity(value);
-  };
-
-  const saveOpacityChange = () => {
-    setConfigField("opacity", useOpacity);
-  };
-
   return (
-    <SettingsItem
-      loading={isLoading}
-      vertical={false}
-      disabled={initialConfig?.windowEffect === 1}
-    >
-      <div>
-        <SettingLabel>
-          {t("settings.setting.window_opacity.label")}
-        </SettingLabel>
-        <SettingDescription>
-          {t("settings.setting.window_opacity.description")}
-        </SettingDescription>
-      </div>
-      <SettingContent>
-        <div className="flex gap-2">
-          <div>50%</div>
-          <Slider
-            onValueChange={(value) => {
-              handleOpacityChange(value[0]);
-            }}
-            onPointerUp={saveOpacityChange}
-            defaultValue={[useOpacity]}
-            min={50}
-            max={100}
-            step={1}
-            className={"w-64 cursor-pointer"}
-          />
-          <div>100%</div>
-          <div className="w-16 font-bold text-center">({useOpacity}%)</div>
-        </div>
-      </SettingContent>
-    </SettingsItem>
+    <SliderConfig
+      configKey="opacity"
+      label={t("settings.setting.window_opacity.label")}
+      description={t("settings.setting.window_opacity.description")}
+      min={50}
+      max={100}
+      step={1}
+      onChange={(value) => {
+        document.documentElement.style.setProperty(
+          "--opacity",
+          String(value / 100)
+        );
+      }}
+    />
   );
 }
--- frontend/src/components/SettingItems/WindowScaleSetting.tsx
+++ frontend/src/components/SettingItems/WindowScaleSetting.tsx
@@ -1,57 +1,19 @@
-import {
-  SettingsItem,
-  SettingContent,
-  SettingDescription,
-  SettingLabel,
-} from "../ui/settings-group";
-import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Slider } from "../ui/my-slider";
-import { useConfig } from "@/contexts/config-provider";
+import { SliderConfig } from "./Presets/SliderConfig";
 
 export function WindowScaleSetting() {
-  const { config, setConfigField } = useConfig();
   const { t } = useTranslation();
-  const [isLoading, setIsLoading] = useState(true);
-  const [useScale, setUseScale] = useState(-1);
-
-  useEffect(() => {
-    if (config && config.windowScale !== undefined && isLoading) {
-      setUseScale(config.windowScale);
-
-      setIsLoading(false);
-    }
-  }, [config?.windowScale]);
-
-  const handleScaleSave = () => {
-    setConfigField("windowScale", useScale);
-    document.documentElement.style.fontSize = useScale * (16 / 100) + "px";
-  };
-
   return (
-    <SettingsItem loading={isLoading}>
-      <div>
-        <SettingLabel>{t("settings.setting.window_scale.label")}</SettingLabel>
-        <SettingDescription>
-          {t("settings.setting.window_scale.description")}
-        </SettingDescription>
-      </div>
-      <SettingContent>
-        <div className="flex gap-2">
-          <div>50%</div>
-          <Slider
-            onValueChange={(value) => setUseScale(value[0])}
-            onPointerUp={handleScaleSave}
-            defaultValue={[useScale]}
-            min={50}
-            max={150}
-            step={10}
-            className={"w-64 cursor-pointer"}
-          />
-          <div>150%</div>
-          <div className="w-16 font-bold text-center">({useScale}%)</div>
-        </div>
-      </SettingContent>
-    </SettingsItem>
+    <SliderConfig
+      configKey="windowScale"
+      label={t("settings.setting.window_scale.label")}
+      description={t("settings.setting.window_scale.description")}
+      min={50}
+      max={150}
+      step={10}
+      onSave={(value) => {
+        document.documentElement.style.fontSize = value * (16 / 100) + "px";
+      }}
+    />
   );
 }
