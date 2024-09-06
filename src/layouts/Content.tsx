e312a1eacda41d8963a671a9e0de3a9419213c56
Bedirhan Yenilmez <yenilmezbedirhan@gmail.com>
2024-09-06 13:32:13 +0300

    change download button to an anchor

--- src/layouts/Content.tsx
+++ src/layouts/Content.tsx
@@ -2,11 +2,6 @@ import { DownloadIcon } from "lucide-react";
 import screenshot1 from "../assets/screenshot-1.png";
 
 export function Content() {
-  const handleDownload = () => {
-    window.location.href =
-      "https://github.com/beyenilmez/iconium/releases/latest/download/iconium-amd64-installer.exe";
-  };
-
   return (
     <div className="relative flex flex-col flex-grow px-4 md:px-8 container">
       <div className="relative z-0 inset-0 flex justify-center items-center">
@@ -22,13 +17,14 @@ export function Content() {
                 gives you the power to bring your icons to life.
               </p>
             </div>
-            <button
+            <a
+              href="https://github.com/beyenilmez/iconium/releases/latest/download/iconium-amd64-installer.exe"
+              download
               className="flex justify-center items-center gap-2 bg-gray-200 hover:shadow-xl rounded-xl h-16 font-semibold text-gray-800 hover:scale-[101%] text-md hover:text-[#6253a0] transition-all"
-              onClick={handleDownload}
             >
               <DownloadIcon className="w-8 h-8" />
               Download for Windows
-            </button>
+            </a>
           </div>
           <img
             src={screenshot1}
